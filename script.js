c07f3e2013fb9d4fa505f04e88af1d4326a68dd1
Intuitive Harmony <j.horst77@gmail.com>
2023-08-19 10:44:25 -0600

    local storage init

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-$(() => {
-  const updateLocalTime = () => {
-    const currentTime = new Date();
-    const options = {
-      hour: "numeric",
-      minute: "numeric",
-
-      timeZoneName: "long",
-    };
-    const formattedTime = currentTime.toLocaleTimeString(undefined, options);
-    $(".local-time").text(formattedTime);
+const getTime = () => {
+  const currentTime = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
   };
+  return currentTime.toLocaleTimeString(undefined, options);
+};
 
+const updateLocalTime = () => {
+  $(".local-time").text(getTime);
+};
+
+const addTeam = () => {
+  const team = {
+    name: "Test Team",
+    members: [
+      {
+        name: "Jason",
+        location: "Denver",
+        timeZone: "",
+      },
+      {
+        name: "Harley",
+        location: "Dream Land",
+        timeZone: "",
+      },
+    ],
+  };
+  localStorage.setItem(`Team: ${team}: `, JSON.stringify(team));
+  console.log(team);
+};
+
+$(() => {
   // Update the time immediately when the page loads
   updateLocalTime();
-
   // Update the time every second
   setInterval(updateLocalTime, 1000);
+
+  // Add Member Button
+  $(".add-member").click(addTeam);
 });
