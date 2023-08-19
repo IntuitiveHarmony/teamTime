318757a68cc08052a854df3945d3c2b5524f3b0a
Intuitive Harmony <j.horst77@gmail.com>
2023-08-18 23:47:59 -0600

    time cards

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-if ($) {
-  console.log("sweet");
-}
+$(() => {
+  const updateLocalTime = () => {
+    const currentTime = new Date();
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+
+      timeZoneName: "long",
+    };
+    const formattedTime = currentTime.toLocaleTimeString(undefined, options);
+    $(".local-time").text(formattedTime);
+  };
+
+  // Update the time immediately when the page loads
+  updateLocalTime();
+
+  // Update the time every second
+  setInterval(updateLocalTime, 1000);
+});
