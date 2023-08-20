30fd1db36b72fb893cab21b515cb0d64ab82e925
Intuitive Harmony <j.horst77@gmail.com>
2023-08-19 22:53:00 -0600

    member modal start

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Time Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const getTime = () => {
   const currentTime = new Date();
   const options = {
@@ -13,6 +16,9 @@ const updateLocalTime = () => {
   $(".local-time").text(getTime);
 };
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Team Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Generate Names
 const nouns = [
   "Dogs",
@@ -98,6 +104,7 @@ const adjectives = [
   "Dippy",
   "Screwball",
 ];
+
 const generateTeamName = () => {
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
   const randomAdjective =
@@ -110,11 +117,11 @@ const showTeamModal = () => {
   $(".team-modal").removeClass("hide");
   $(".teams-container").addClass("hide");
 };
+
 const hideTeamModal = () => {
   $(".team-modal").addClass("hide");
   $(".team-validate").addClass("hide");
   $("#team-name").val("");
-  displayTeams();
 };
 
 const showTeams = () => {
@@ -138,14 +145,17 @@ const displayTeams = () => {
 
   const teamsJSON = localStorage.getItem("teams");
   const existingTeams = JSON.parse(teamsJSON);
-
+  // Loop that lays down the team buttons
   for (let i = 0; i < existingTeams.length; i++) {
+    const teamName = existingTeams[i].name;
+    const teamMembers = existingTeams[i].members;
+
     const newTimeCard = $("<li>")
       .addClass("team-button")
       .attr("id", `team-${i}`);
     const nameContainer = $("<div>")
       .addClass("team-name-container")
-      .text(`${existingTeams[i].name}`)
+      .text(`${teamName}`)
       .appendTo(newTimeCard);
     const memberContainer = $("<div>")
       .addClass("team-member-container")
@@ -153,14 +163,12 @@ const displayTeams = () => {
       .appendTo(newTimeCard);
     $(".teams-index").append(newTimeCard);
     // Single out individual team's onClick
+    // "Show View" for each team
     $(`#team-${i}`).on("click", () => {
+      console.log(teamMembers);
       $(".team-show-container").removeClass("hide");
-      // "Show route" for each team
-      const team = $("<div>")
-        .addClass("member-card")
-        .text(existingTeams[i].name);
       $(".teams-container").addClass("hide");
-      $(".team-show-container").empty().append(team);
+      $(".team-header").text(teamName);
     });
   }
 };
@@ -182,6 +190,13 @@ const submitTeam = () => {
   }
 };
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Member Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const showMemberModal = () => {
+  console.log("memeber modal");
+};
+
 $(() => {
   initLocalStorage();
   // Update the time immediately when the page loads
@@ -197,4 +212,7 @@ $(() => {
   $(".cancel-team").click(hideTeamModal);
   $(".submit-team").click(submitTeam);
   $(".generate-team").click(generateTeamName);
+
+  // Member Buttons
+  $("#add-member").click(showMemberModal);
 });
